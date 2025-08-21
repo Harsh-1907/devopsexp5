c08bd7255c97d87431718d1cf8dbbdde6cb8fe98
tejas.avhad <tejas.avhad@somaiya.edu>
2025-08-21 12:42:43 +0530

    Updated HelloWorld

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,1 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello from GitHub-Jenkins Integration!");
-    }
-}
+System.out.println("Hello Jenkins, new build triggered!");
